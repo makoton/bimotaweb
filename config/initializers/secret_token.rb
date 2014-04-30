8046329f9e9b9e0fd09e16349413f3a3cb7cc8a5
sebastian <sebastian.stockebrand@gmail.com>
2014-04-30 17:55:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bimotaweb::Application.config.secret_token = '0728892bc2d8017511f91df951ea8e3ddc44e1cceb314baf9d4614de18f3608caf62101ad67db0a4f15861f3c1ef526919479efb67c923aec103becb9dbb986f'
