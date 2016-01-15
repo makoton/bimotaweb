4a378c00492bed6574a5e8f00bf20159df38de4e
nicolas serrat <nicolas@modyo.com>
2016-01-15 14:27:02 -0300

    lalalala

--- app/controllers/admin/vehicles_controller.rb
+++ app/controllers/admin/vehicles_controller.rb
@@ -6,14 +6,14 @@ class Admin::VehiclesController < Admin::BaseController
   def index
     @page_title = 'Vehículos'
     if params[:owner_id].present?
-      @vehicles = Vehicle.where(user_id: params[:owner_id]).order('created_at DESC').page params[:page]
+      @vehicles = BikeVehicle.where(user_id: params[:owner_id]).order('created_at DESC').page params[:page]
     elsif params[:query]
         # This complex query should get the results by name no mather what spanish character was entered.
       conditions = ["( REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(LOWER(vehicles.model),'á', 'a'),'é', 'e'),'í', 'i'),'ó', 'o'),'ú', 'u'),'ñ', 'n')  LIKE ? OR LOWER(vehicles.license_plate) LIKE ? OR LOWER(vehicles.chassis_number) LIKE ? OR LOWER(bike_brands.name) LIKE ? )", "%#{params[:query].downcase}%", "%#{params[:query].downcase}%", "%#{params[:query].downcase}%", "%#{params[:query].downcase}%"]
 
         @vehicles = BikeVehicle.joins(:bike_brand).where(conditions).page params[:page]
     else
-      @vehicles = Vehicle.order('created_at DESC').page params[:page]
+      @vehicles = BikeVehicle.order('created_at DESC').page params[:page]
     end
   end
 
